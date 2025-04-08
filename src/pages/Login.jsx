6b322d5bfd7d9685771c427e5abfa44a98856ffd
kosalaprasetya <kosalaprasetya@gmail.com>
2025-04-08 21:00:44 +0700

    feat: trying post to rest api login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,33 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router';
+import http from '../helpers/http';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await http({
+        method: 'POST',
+        url: '/auth/login',
+        data: {
+          email,
+          password,
+        },
+      });
+      localStorage.setItem('access_token', result.data.token);
+      console.log(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="bg-slate-900 h-screen w-screen flex justify-center items-center">
-      <form className="max-w-sm mx-auto w-lg p-4">
+      <form
+        className="max-w-sm mx-auto w-lg p-4"
+        onSubmit={handleLogin}
+      >
         <h2 className="dark:text-white font-bold text-2xl mb-4 text-center">Login</h2>
         <div className="mb-5">
           <label
