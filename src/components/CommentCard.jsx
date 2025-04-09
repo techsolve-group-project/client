4fc9c453f0327b9da0d9f40e033645f1a73cae76
hafidzahm <hafizhahm123@gmail.com>
2025-04-09 11:08:38 +0700

    feat: Update CommentCard to display user's name in the comment

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -37,19 +37,11 @@ export default function CommentCard() {
             </svg>
           </button>
         </div>
-// <<<<<<< feat/comment-textarea
-        <div className="flex items-center space-x-4">
-          <div className="text-right">
-            <p className="text-xs">
-              Tempel nama kamu disini commented{" "}
-              <time dateTime="2020-08-06T20:01:00">Aug 6, 2020 at 20:01</time>
-// =======
-//         <div className="flex-1">
-//           <div className="mt-4 py-2 px-3 bg-green-100 border border-green-300 rounded-md">
-//             <p className="text-green-700 leading-relaxed mt-1">
-//               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum corrupti, nam aliquid accusantium velit voluptas praesentium voluptates. Odio consectetur aut dicta deleniti? Vitae, quidem impedit. Soluta magni vero nulla
-//               commodi.
-// >>>>>>> dev
+        <div className="flex-1">
+          <div className="mt-4 py-2 px-3 bg-green-100 border border-green-300 rounded-md">
+            <p className="text-green-700 leading-relaxed mt-1">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum corrupti, nam aliquid accusantium velit voluptas praesentium voluptates. Odio consectetur aut dicta deleniti? Vitae, quidem impedit. Soluta magni vero nulla
+              commodi.
             </p>
           </div>
           <div className="flex items-center justify-between mt-4 text-sm text-gray-500">
@@ -57,7 +49,7 @@ export default function CommentCard() {
             <div className="flex items-center space-x-4">
               <div className="text-right">
                 <p className="text-xs">
-                  created <time dateTime="2020-08-06T20:01:00">Aug 6, 2020 at 20:01</time>
+                  Namamu commented <time dateTime="2020-08-06T20:01:00">Aug 6, 2020 at 20:01</time>
                 </p>
                 <div className="flex items-center"></div>
               </div>
